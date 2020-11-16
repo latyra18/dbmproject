23b4a5ca8d522ed72154feb7298527094067b1b3
kbenjamin4 <kbenjamin4@student.pvamu.edu>
2020-11-15 19:41:59 -0600

    set structure to make project more modular. Connected to db and tested query with dummy data. Going to work on finishing db models and adding routes.py

--- panthergrill/flaskProject/app/models.py
+++ panthergrill/flaskProject/app/models.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String, Date, Enum, Float
+from sqlalchemy import Column, Integer, String, Date, Enum, Float, Time
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 # Table users
 
@@ -32,4 +33,44 @@ class Employee(Base):
     phoneNumber = Column(String(45), nullable=False)
     endDate = Column(Date, nullable=True)
 
+
 # Table fooditems
+
+
+class Fooditems(Base):
+    __tablename__ = 'fooditems'
+    fooditemsID = Column(Integer, primary_key=True)
+    idOrder = Column(Integer, nullable=False)
+    idItem = Column(Integer, nullable=False)
+
+
+# Table location
+class Location(Base):
+    __tablename__ = 'location'
+    idLocation = Column(Integer, primary_key=True)
+    address = Column(String(776), nullable=False)
+    hoursOpen = Column(Time, nullable=False)
+    hoursCLose = Column(Time, nullable=False)
+
+
+# Table menu
+class Menu(Base):
+    __tablename__ = 'menu'
+    idItem = Column(Integer, primary_key=True)
+    foodName = Column(String(45), nullable=False)
+    price = Column(Float, nullable=False)
+
+
+# Table shifts
+class Shifts(Base):
+    __tablename__ = 'shifts'
+    idShift = Column(Integer, primary_key=True)
+    shiftType = Column('shiftType', Enum('breakfast', 'lunch', 'dinner'))
+    shiftStart = Column(Time, nullable=False)
+    shiftEnd = Column(Time, nullable=False)
+    shiftHours = Column(Time, nullable=False)
+
+
+# Table orders
+
+# Table schedule
